98d912a8307838d5faa0d06bc024bb4083b67487
PRANKBOTS <33993971+Aprank@users.noreply.github.com>
2018-02-14 22:20:27 +0700

    Update LineApi.py

--- PRANKBOTS/LineApi.py
+++ PRANKBOTS/LineApi.py
@@ -24,7 +24,7 @@ class LINE:
   def __init__(self):
     self.Talk = Talk()
     self._session = requests.session()
-    self._headers = {'X-Line-Application': 'DESKTOPMAC\t10.10.2-YOSEMITE-x64\tMAC 4.5.0', 'X-Line-Access': self.authToken, 'User-Agent': 'Line/8.0.0 iPad4,1 9.0.2'}
+    self._headers = {'X-Line-Application': 'IOSIPAD\t7.18.0\tiPhone OS\t11.12.1', 'X-Line-Access': self.authToken, 'User-Agent': 'Line/8.0.0 iPad4,1 9.0.2'}
 
   def login(self, mail=None, passwd=None, cert=None, token=None, qr=False, callback=None):
     if callback is None:
@@ -44,7 +44,7 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'DESKTOPMAC\t10.10.2-YOSEMITE-x64\tMAC 4.5.0',
+              'X-Line-Application': 'IOSIPAD\t7.18.0\tiPhone OS\t11.12.1',
               'X-Line-Access': self.authToken,
               'User-Agent': 'Line/8.0.0'
    }
